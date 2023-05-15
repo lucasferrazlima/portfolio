fe332564b030bc087c1a18c99c4b352ad1c71242
Lucas <lucasferrazl@hotmail.com>
2023-05-15 20:50:40 -0300

    Add basic structure to page

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#333343',
+        secondary: '#f0eee9',
+      },
+    },
+    plugins: [],
+  },
+};
